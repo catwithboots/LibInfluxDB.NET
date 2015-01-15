207dc5d9ae12abff2aaef5c8b7346e2fd4e19728
kmeinster@schubergphilis.com <kmeinster@schubergphilis.com>
2015-01-15 19:15:18 +0100

    Initial Commit
    Based on https://github.com/ziyasal/InfluxDB.Net but now not portable
    (unbuildable in windows actually)

---
+++ InfluxDB.NET/InfluxDbResponse.cs
@@ -0,0 +1,49 @@
+﻿using System.Net;
+
+namespace InfluxDB.Net
+{
+    public class InfluxDbApiResponse
+    {
+        public InfluxDbApiResponse(HttpStatusCode statusCode, string body)
+        {
+            StatusCode = statusCode;
+            Body = body;
+        }
+
+        public HttpStatusCode StatusCode { get; private set; }
+
+        public string Body { get; private set; }
+
+        public virtual bool Success
+        {
+            get { return StatusCode == HttpStatusCode.OK; }
+        }
+    }
+
+    public class InfluxDbApiCreateResponse : InfluxDbApiResponse
+    {
+        public InfluxDbApiCreateResponse(HttpStatusCode statusCode, string body)
+            : base(statusCode, body)
+        {
+        }
+
+        public override bool Success
+        {
+            get { return StatusCode == HttpStatusCode.Created; }
+        }
+    }
+
+    public class InfluxDbApiDeleteResponse : InfluxDbApiResponse
+    {
+        public InfluxDbApiDeleteResponse(HttpStatusCode statusCode, string body)
+            : base(statusCode, body)
+        {
+        }
+
+        public override bool Success
+        {
+            //TODO: Ask to influx db creators
+            get { return StatusCode == HttpStatusCode.NoContent; }
+        }
+    }
+}
